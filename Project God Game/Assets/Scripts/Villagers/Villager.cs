ae0056b98b9ad683d301bc84aa041b2b0683f079
salfalahi <s.alfalahi@saedubai.com>
2019-10-21 11:27:55 +0400

    Testing the production script and the villagers script, need to consult Abubaker

---
+++ Project God Game/Assets/Scripts/Villagers/Villager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Villager : MonoBehaviour
+{
+    public VillagerStats stats;
+
+    void Start()
+    {
+        stats.Str = Random.Range(1, 3);
+        stats.Dex = Random.Range(1, 3);
+        stats.Int = Random.Range(1, 3);
+        stats.Fth = Random.Range(1, 3);
+
+        stats.level = 1;
+
+        stats.villager = gameObject;
+    }
+}
