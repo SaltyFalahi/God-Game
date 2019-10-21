3ed27228442dba6ac70a245bcb05f42b4cbca09b
salfalahi <s.alfalahi@saedubai.com>
2019-10-21 09:33:45 +0400

    Added base scriptable objects scripts from video, will expand as needed

---
+++ Project God Game/Assets/Scripts/Scriptable Objects/FloatReference.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FloatReference
+{
+    public bool useConstant = true;
+    public float constantValue;
+    public FloatVariable Variable;
+
+    public float Value
+    {
+        get { return useConstant ? constantValue : Variable.Value; }
+    }
+}
---
+++ Project God Game/Assets/Scripts/Scriptable Objects/GameEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListeners> listeners = new List<GameEventListeners>();
+
+    public void Raise()
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventRaised();
+        }
+    }
+
+    public void RegisterListener(GameEventListeners listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void UnRegisterListener(GameEventListeners listener)
+    {
+        listeners.Remove(listener);
+    }
+}
