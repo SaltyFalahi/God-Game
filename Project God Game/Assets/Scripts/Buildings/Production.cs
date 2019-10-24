2aaa5e02f0cfbf8fb33d105b2642224701b2d6ca
salfalahi <s.alfalahi@saedubai.com>
2019-10-24 14:13:50 +0400

    Consulted Gamal and Abubaker, finished the villager and building stats, finished production script

---
+++ Project God Game/Assets/Scripts/Buildings/Production.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Production : MonoBehaviour
+{
+    public enum AllStats
+    {
+        Str,
+        Dex,
+        Int,
+        Fth
+    }
+
+    public AllStats mainStat;
+
+    public FloatVariable resource;
+
+    public Villager stats;
+
+    public int lvl = 1;
+
+    public float countdown;
+
+    private int productionStat;
+
+    private float timer;
+
+    void Update()
+    {
+        if (stats != null)
+        {
+            switch (mainStat)
+            {
+                case AllStats.Str:
+                    productionStat = stats.Str;
+                    break;
+
+                case AllStats.Dex:
+                    productionStat = stats.Dex;
+                    break;
+
+                case AllStats.Int:
+                    productionStat = stats.Int;
+                    break;
+
+                case AllStats.Fth:
+                    productionStat = stats.Fth;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        if (timer <= 0)
+        {
+            resource.Value += (lvl + productionStat) / 2;
+            timer = countdown;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Villager")
+        {
+            stats = other.gameObject.GetComponent<Villager>();
+            timer -= Time.deltaTime;
+        }
+    }
+}
--- Project God Game/Assets/Scripts/Villagers/Villager.cs
+++ Project God Game/Assets/Scripts/Villagers/Villager.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Villager : MonoBehaviour
 {
-    public VillagerStats stats;
+    public int Str;
+    public int Dex;
+    public int Int;
+    public int Fth;
+
+    public int Lvl;
 
     void Start()
     {
-        stats.Str = Random.Range(1, 3);
-        stats.Dex = Random.Range(1, 3);
-        stats.Int = Random.Range(1, 3);
-        stats.Fth = Random.Range(1, 3);
+        Str = Random.Range(1, 3);
+        Dex = Random.Range(1, 3);
+        Int = Random.Range(1, 3);
+        Fth = Random.Range(1, 3);
 
-        stats.level = 1;
-
-        stats.villager = gameObject;
+        Lvl = 1;
     }
 }
