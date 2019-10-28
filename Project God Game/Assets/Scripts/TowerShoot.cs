64b35ce6ac82393b2d46509963f278860ac14c90
Madshtik <jann_cosio@yahoo.com>
2019-10-29 03:09:17 +0400

    Added te following scripts:
    +Bullet (will make the Transform.translate into AddForce soon)
    
    +HealthTest (just so I can check if the bullets are affecting enemy HP)
    
    +ObjectPool (I wanted to do ObjectPooling but after what Ahmed said today, imma leave it empty)
    
    +TargetingController (only does the targeting logic)
    
    +TowerShoot (should be named better but it basically changes the tower type so it'll shoot the correct bullet)
    
    Other additions:
    
    +Made a prefab folder with 2 sphere prefabs
    
    +Made 3 new materials for testing purposes

---
+++ Project God Game/Assets/Scripts/TowerShoot.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShoot : MonoBehaviour
+{
+    public Transform shootPos;
+    public GameObject bullet;
+    public float shotTimer;
+
+    TargetingController targetingController;
+
+    public enum TurretType
+    {
+        Single,
+        AOE,
+        Freeze,
+        Slow
+    }
+
+    public TurretType turretType;
+
+    void Start()
+    {
+        targetingController = GetComponent<TargetingController>();
+    }
+
+    void Update()
+    {
+        switch (turretType)
+        {
+            case TurretType.Single:
+                if (targetingController.hasTarget)
+                {
+                    shotTimer -= 1 * Time.deltaTime;
+                    if (shotTimer <= 0f)
+                    {
+                        Instantiate(bullet, new Vector3(shootPos.position.x, shootPos.position.y, shootPos.position.z), transform.rotation);
+                        shotTimer = 3f;
+                    }
+
+                }
+                print("Single Tower");
+                break;
+
+            case TurretType.AOE:
+                if (targetingController.hasTarget)
+                {
+                    shotTimer -= 1 * Time.deltaTime;
+                    if (shotTimer <= 0f)
+                    {
+                        Instantiate(bullet, new Vector3(shootPos.position.x, shootPos.position.y, shootPos.position.z), transform.rotation);
+                        shotTimer = 5f;
+                    }
+                }
+                print("AOE Tower");
+                break;
+
+            case TurretType.Freeze:
+                print("Freeze Tower");
+                break;
+
+            case TurretType.Slow:
+                print("Slow Tower");
+                break;
+        }
+    }
+}
