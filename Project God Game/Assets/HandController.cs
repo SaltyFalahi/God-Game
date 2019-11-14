88c8915ec5afd47c3531b85762e23fff93a4c5ce
aryanben <a.bendrey@saedubai.com>
2019-11-14 15:03:59 +0400

    Setting up VR WIP

---
+++ Project God Game/Assets/HandController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("XRI_Left_TriggerButton"))
+        {
+            var cols = Physics.OverlapSphere(this.transform.position, 3);
+            if (cols.Length>0)
+            {
+                if (cols[0].gameObject.GetComponentInParent<Pickable>())
+                {
+                    cols[0].transform.parent = this.transform;
+                     
+                }
+            }
+        }
+    }
+}
