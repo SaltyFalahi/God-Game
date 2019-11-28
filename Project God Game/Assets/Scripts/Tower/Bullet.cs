717d78cb565818d625b383eb310ddbf42b337f1b
Madshtik <jann_cosio@yahoo.com>
2019-11-28 17:54:28 +0400

    Improved the tower scripts

---
+++ Project God Game/Assets/Scripts/Tower/Bullet.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public enum type
+    {
+        Single,
+        AOE,
+        Freeze,
+        Slow
+    }
+
+    public type current;
+
+    public LayerMask collisionLayer;
+
+    public float myBulletTimer;
+    public float bulletForce;
+
+    public int singleDam;
+    public int AOEDam;
+    public int AOERadius;
+
+    Rigidbody bulletRb;
+
+    Transform target;
+
+    Vector3 direction;
+
+    int damage;
+
+    private IEnumerator Start()
+    {
+        bulletRb = GetComponent<Rigidbody>();
+
+        direction = (target.position - transform.position);
+
+        bulletRb.AddForce(bulletForce * direction);
+
+        yield return new WaitForSeconds(myBulletTimer);
+
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Target(Transform target)
+    {
+        this.target = target;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            switch (current)
+            {
+                case type.Single:
+                    {
+                        damage = singleDam;
+                        target.SendMessage("Damage", damage);
+                    }
+                        break;
+
+                case type.AOE:
+                    {
+                        damage = AOEDam;
+
+                        var cols = Physics.OverlapSphere(transform.position, AOERadius, collisionLayer);
+
+                        for (int i = 0; i < cols.Length; i++)
+                        {
+                            if (cols[i].gameObject.GetComponentInParent<Enemy>())
+                            {
+                                cols[i].SendMessage("Damage", damage);
+                            }
+                        }
+                    }
+                    break;
+
+                case type.Freeze:
+                    {
+                        damage = singleDam / 2;
+                        target.SendMessage("Freeze", damage);
+                    }
+                    break;
+
+                case type.Slow:
+                    {
+                        damage = AOEDam / 2;
+
+                        var cols = Physics.OverlapSphere(transform.position, AOERadius, collisionLayer);
+
+                        for (int i = 0; i < cols.Length; i++)
+                        {
+                            if (cols[i].gameObject.GetComponentInParent<Enemy>())
+                            {
+                                cols[i].SendMessage("Slow", damage);
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+
+        
+    }
+}
---
+++ Project God Game/Assets/Scripts/Tower/TowerShoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShoot : MonoBehaviour
+{
+    public GameObject bullet;
+
+    public float shotTimer;
+
+    Transform target;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            shotTimer -= 1 * Time.deltaTime;
+            if (shotTimer <= 0f)
+            {
+                var bulletInstance = Instantiate(bullet, transform.position, transform.rotation);
+
+                bulletInstance.SendMessage("Target", target);
+               
+                shotTimer = 3f;
+            }
+        }
+    }
+
+    void Shoot(Transform target)
+    {
+        this.target = target;
+    }
+}
