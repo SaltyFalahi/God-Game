da05cb37c9cb76f8581b6fd30e79784dd635c983
SaltyFalahi <suliflin@gmail.com>
2019-12-06 13:39:15 +0400

    Added placement script, added building script, added request scriptable objects for buildings, added names for male and female villagers

--- Project God Game/Assets/Scripts/Placement.cs
+++ Project God Game/Assets/Scripts/Placement.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class Placement : MonoBehaviour
 {
+    public GameObject phantom;
+
+    public Transform point;
+
+    public Rigidbody rb;
+
+    public NodeGrid grid;
+
+    public Building building;
+
+    public bool held;
+
     void Start()
     {
-        
+        building = GetComponent<Building>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        RaycastHit hit;
+
+        if (!held)
+        {
+            rb.isKinematic = false;
+
+            Debug.DrawLine(transform.position, -Vector3.up * 10);
+
+            if (Physics.Raycast(transform.position, -Vector3.up, out hit, 10))
+            {
+                rb.isKinematic = true;
+
+                phantom.transform.position = grid.NodeFromWorldPoint(point.position).nodeWorldPos;
+
+                building.placed = true;
+
+                building.RequestResource(building.progress[0].count, building.progress[0].name);
+                building.index++;
+            }
+        }
     }
 }
