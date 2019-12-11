415a8882e1bd1a81102017df744f8e3795534a0e
rayane <iskull117@gmail.com>
2019-12-11 21:53:52 +0400

    all done but the menu

---
+++ Studio 2 GreyBox/Assets/Rayane's Code/Rocket.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    
+
+    private Rigidbody rb;
+
+    float timeframe;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 300);
+    }
+	
+	// Update is called once per frame
+
+}
