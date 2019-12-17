ec43fb2000185f0d3d38ce429268b7b911181b1c
Madshtik <jann_cosio@yahoo.com>
2019-12-17 15:49:42 +0400

    Bruh

--- Project God Game/Assets/Scripts/DeleteAfterSC/WaterShader.cs
+++ Project God Game/Assets/Scripts/DeleteAfterSC/WaterShader.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class WaterShader : MonoBehaviour
 {
-    public Mesh myMesh;
-
     public float waveAmplitude;
     public float waveSpeed;
     public float waveFrequency;
+
+    Mesh myMesh;
+
+    Vector3[] verts;
 
     float waterHeight = 10;
     float waterWidth = 500;
@@ -16,12 +18,17 @@ public class WaterShader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        myMesh = GetComponent<MeshFilter>().mesh;
     }
 
     // Update is called once per frame
     void Update()
     {
+        verts = myMesh.vertices;
 
+        for (int i = 0; i < verts.Length; i++)
+        {
+            verts[i].y += Mathf.Sin(Time.deltaTime * waveSpeed + (verts[i].x * verts[i].z * waveFrequency) * waveAmplitude);
+        }
     }
 }
